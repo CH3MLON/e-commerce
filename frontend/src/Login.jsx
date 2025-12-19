6814588a4817c441df7b0c673df67bec34266221
CH3MLON <rohithkarthik275@gmail.com>
2025-12-19 09:07:47 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -16,7 +16,7 @@ export default function Login({ setUser, loadCart }) {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://3.85.183.248:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
